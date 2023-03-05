8c1ddbe6a9351f648d133ea32cce8788953d13b5
iVANECHKA <akk.company2014@gmail.com>
2023-03-06 00:18:19 +0300

    Сделал вторую версию шапки и анимировал

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       const header = document.querySelector('.header');
+      const headerLogo = document.querySelector('.header-logo');
+      const headerNumber = document.querySelector('.header-number');
+      const headerMail = document.querySelector('.header-mail');
+      const headerMenuLinks = document.querySelectorAll('.header-menu-link');
   
       if (entry.isIntersecting) {
-        header.classList.add('header-black');
-        return; // if we added the class, exit the function
+        header.classList.remove('header-white');
+        headerLogo.classList.remove('header-logo-white');
+        headerNumber.classList.remove('header-number-white');
+        headerMail.classList.remove('header-mail-white');
+        headerMenuLinks.forEach((link) => {
+          link.classList.remove('header-menu-link-white');
+        });
+        document.querySelector('.header-viber').src = './images/viber.svg';
+        document.querySelector('.header-facebook').src = './images/facebook.svg';
+        document.querySelector('.header-vk').src = './images/vk.svg';
+        document.querySelector('.header-arrow').src = './images/arrow-down-white.svg';
+        return;
       }
   
-      // We're not intersecting, so remove the class!
-      header.classList.remove('header-black');
+
+      header.classList.add('header-white');
+      headerLogo.classList.add('header-logo-white');
+      headerNumber.classList.add('header-number-white');
+      headerMail.classList.add('header-mail-white');
+      headerMenuLinks.forEach((link) => {
+        link.classList.add('header-menu-link-white');
+      });
+      document.querySelector('.header-viber').src = './images/viber-dark.svg';
+      document.querySelector('.header-facebook').src = './images/facebook-dark.svg';
+      document.querySelector('.header-vk').src = './images/vk-dark.svg';
+      document.querySelector('.header-arrow').src = './images/arrow-down-header-black.svg';
     });
   });
   
-  observer.observe(document.querySelector('.feed-show-all-link'));
+  observer.observe(document.querySelector('.header-anchor'));
+
